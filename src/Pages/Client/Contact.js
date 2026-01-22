a4561b79fce1d563c094096b763b5b36d2f1e0c5
Helie96 <ha5984448@gmail.com>
2026-01-22 22:41:42 +0700

    Contact.js

--- src/Pages/Client/Contact.js
+++ src/Pages/Client/Contact.js
@@ -6,30 +6,45 @@ import Spinner from "../../Components/Client/Spinner";
 import { DangerAlert, SuccessAlert } from "../../Components/Alert/Alert";
 import { Link } from "react-router-dom";
 
-
 export default function Contact() {
-  const { register, handleSubmit, formState: { errors }, reset } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm();
   const dispatch = useDispatch();
   const [isLoading, setIsLoading] = useState(false); // Thêm state loading
-  const [alert, setAlert] = useState({ open: false, message: '', severity: '' });
+  const [alert, setAlert] = useState({
+    open: false,
+    message: "",
+    severity: "",
+  });
 
   const onSubmit = async (data) => {
     setIsLoading(true); // Bắt đầu loading
     try {
       setIsLoading(false); // Kết thúc loading
       await dispatch(addNewContact(data)); // Gọi action để gửi thông tin liên hệ
-      setAlert({ open: true, message: 'Gửi thông tin thành công!', severity: 'success' });
+      setAlert({
+        open: true,
+        message: "Message sent successfully!",
+        severity: "success",
+      });
       reset();
     } catch (error) {
-      console.error('Error sending contact data:', error);
-      setAlert({ open: true, message: 'Gửi thông tin thất bại!', severity: 'error' });
+      console.error("Error sending contact data:", error);
+      setAlert({
+        open: true,
+        message: "Failed to send message!",
+        severity: "error",
+      });
     }
   };
 
   const handleCloseAlert = () => {
     setAlert({ ...alert, open: false });
   };
-
 
   return (
     <div>
@@ -40,18 +55,18 @@ export default function Contact() {
           <div className="container-fluid p-0 py-5 bg-dark hero-header mb-5">
             <div className="container text-center my-5 pt-5 pb-4">
               <h1 className="display-3 text-white mb-3 animated slideInDown">
-                Liên hệ với chúng tôi
+                Contact Us
               </h1>
               <nav aria-label="breadcrumb">
                 <ol className="breadcrumb justify-content-center text-uppercase">
                   <li className="breadcrumb-item">
-                    <Link to="/">Trang chủ</Link>
+                    <Link to="/">Home</Link>
                   </li>
                   <li
                     className="breadcrumb-item text-white active"
                     aria-current="page"
                   >
-                    Liên hệ
+                    Contact
                   </li>
                 </ol>
               </nav>
@@ -60,48 +75,54 @@ export default function Contact() {
 
           <div className="container-xxl py-5">
             <div className="container">
-              <div className="text-center wow fadeInUp" data-wow-delay="0.1s">
+              <div
+                className="text-center wow fadeInUp"
+                data-wow-delay="0.1s"
+              >
                 <h5 className="section-title ff-secondary text-center text-primary fw-normal">
-                  Liên hệ với chúng tôi
+                  Contact Us
                 </h5>
-                <h1 className="mb-5">Liên hệ để giải đáp mọi thắc mắc</h1>
+                <h1 className="mb-5">
+                  Get in touch to have all your questions answered
+                </h1>
               </div>
               <div className="row g-4">
                 <div className="col-12">
                   <div className="row gy-4">
                     <div className="col-md-4">
                       <h5 className="section-title ff-secondary fw-normal text-start text-primary">
-                        Đặt chỗ
+                        Reservations
                       </h5>
                       <p>
                         <i className="fa fa-envelope-open text-primary me-2"></i>
-                        contact.huongsen@gmail.com
+                        contact.happycow@gmail.com
                       </p>
                     </div>
                     <div className="col-md-4">
                       <h5 className="section-title ff-secondary fw-normal text-start text-primary">
-                        Thông tin chung
+                        General Information
                       </h5>
                       <p>
                         <i className="fa fa-envelope-open text-primary me-2"></i>
-                        contact.huongsen@gmail.com
+                        contact.happycow@gmail.com
                       </p>
                     </div>
                     <div className="col-md-4">
                       <h5 className="section-title ff-secondary fw-normal text-start text-primary">
-                        Kỹ thuật
+                        Technical Support
                       </h5>
                       <p>
                         <i className="fa fa-envelope-open text-primary me-2"></i>
-                        contact.huongsen@gmail.com
+                        contact.happycow@gmail.com
                       </p>
                     </div>
                   </div>
                 </div>
+
                 <div className="col-md-6 wow fadeIn" data-wow-delay="0.1s">
                   <iframe
                     className="position-relative rounded w-100 h-100"
-                    src="https://www.google.com/maps/d/u/0/embed?mid=1bjw9hlxO6tvUNqAKJSFLdJAHHK_dmlY&ehbc=2E312F&noprof=1"
+                    src="<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3723.8028617611994!2d105.74358217508126!3d21.040572580611695!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x313454f2ad50bd6f%3A0x1e8e0e50817009ec!2zMSBUcuG7i25oIFbEg24gQsO0LCBYdcOibiBQaMawxqFuZywgTmFtIFThu6sgTGnDqm0sIEjDoCBO4buZaSAxMDAwMDAsIFZpZXRuYW0!5e0!3m2!1sen!2s!4v1769096359112!5m2!1sen!2s" 
                     frameBorder="0"
                     style={{ minHeight: "350px", border: 0 }}
                     allowFullScreen=""
@@ -109,6 +130,7 @@ export default function Contact() {
                     tabIndex="0"
                   ></iframe>
                 </div>
+
                 <div className="col-md-6">
                   <div className="wow fadeInUp" data-wow-delay="0.2s">
                     <form onSubmit={handleSubmit(onSubmit)}>
@@ -117,63 +139,101 @@ export default function Contact() {
                           <div className="form-floating">
                             <input
                               type="text"
-                              className={`form-control ${errors.name ? 'is-invalid' : ''}`}
+                              className={`form-control ${
+                                errors.name ? "is-invalid" : ""
+                              }`}
                               id="name"
-                              placeholder="Tên của bạn"
-                              {...register("name", { required: "Tên là bắt buộc" })}
+                              placeholder="Your Name"
+                              {...register("name", {
+                                required: "Name is required",
+                              })}
                             />
-                            <label htmlFor="name">Tên của bạn</label>
-                            {errors.name && <div className="invalid-feedback">{errors.name.message}</div>}
+                            <label htmlFor="name">Your Name</label>
+                            {errors.name && (
+                              <div className="invalid-feedback">
+                                {errors.name.message}
+                              </div>
+                            )}
                           </div>
                         </div>
+
                         <div className="col-md-6">
                           <div className="form-floating">
                             <input
                               type="email"
-                              className={`form-control ${errors.email ? 'is-invalid' : ''}`}
+                              className={`form-control ${
+                                errors.email ? "is-invalid" : ""
+                              }`}
                               id="email"
-                              placeholder="Email của bạn"
+                              placeholder="Your Email"
                               {...register("email", {
-                                required: "Email là bắt buộc",
+                                required: "Email is required",
                                 pattern: {
-                                  value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
-                                  message: "Email không hợp lệ"
-                                }
+                                  value:
+                                    /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+                                  message: "Invalid email address",
+                                },
                               })}
                             />
-                            <label htmlFor="email">Email của bạn</label>
-                            {errors.email && <div className="invalid-feedback">{errors.email.message}</div>}
+                            <label htmlFor="email">Your Email</label>
+                            {errors.email && (
+                              <div className="invalid-feedback">
+                                {errors.email.message}
+                              </div>
+                            )}
                           </div>
                         </div>
+
                         <div className="col-12">
                           <div className="form-floating">
                             <input
                               type="text"
-                              className={`form-control ${errors.subject ? 'is-invalid' : ''}`}
+                              className={`form-control ${
+                                errors.subject ? "is-invalid" : ""
+                              }`}
                               id="subject"
-                              placeholder="Chủ đề"
-                              {...register("subject", { required: "Chủ đề là bắt buộc" })}
+                              placeholder="Subject"
+                              {...register("subject", {
+                                required: "Subject is required",
+                              })}
                             />
-                            <label htmlFor="subject">Chủ đề</label>
-                            {errors.subject && <div className="invalid-feedback">{errors.subject.message}</div>}
+                            <label htmlFor="subject">Subject</label>
+                            {errors.subject && (
+                              <div className="invalid-feedback">
+                                {errors.subject.message}
+                              </div>
+                            )}
                           </div>
                         </div>
+
                         <div className="col-12">
                           <div className="form-floating">
                             <textarea
-                              className={`form-control ${errors.message ? 'is-invalid' : ''}`}
-                              placeholder="Để lại tin nhắn tại đây"
+                              className={`form-control ${
+                                errors.message ? "is-invalid" : ""
+                              }`}
+                              placeholder="Leave a message here"
                               id="message"
                               style={{ height: "150px" }}
-                              {...register("message", { required: "Tin nhắn là bắt buộc" })}
+                              {...register("message", {
+                                required: "Message is required",
+                              })}
                             ></textarea>
-                            <label htmlFor="message">Tin nhắn</label>
-                            {errors.message && <div className="invalid-feedback">{errors.message.message}</div>}
+                            <label htmlFor="message">Message</label>
+                            {errors.message && (
+                              <div className="invalid-feedback">
+                                {errors.message.message}
+                              </div>
+                            )}
                           </div>
                         </div>
+
                         <div className="col-12">
-                          <button className="btn btn-primary w-100 py-3" type="submit">
-                            Gửi tin nhắn
+                          <button
+                            className="btn btn-primary w-100 py-3"
+                            type="submit"
+                          >
+                            Send Message
                           </button>
                         </div>
                       </div>
@@ -185,8 +245,17 @@ export default function Contact() {
           </div>
         </>
       )}
-      <SuccessAlert open={alert.open && alert.severity === 'success'} onClose={handleCloseAlert} message={alert.message} />
-      <DangerAlert open={alert.open && alert.severity === 'error'} onClose={handleCloseAlert} message={alert.message} />
+
+      <SuccessAlert
+        open={alert.open && alert.severity === "success"}
+        onClose={handleCloseAlert}
+        message={alert.message}
+      />
+      <DangerAlert
+        open={alert.open && alert.severity === "error"}
+        onClose={handleCloseAlert}
+        message={alert.message}
+      />
     </div>
   );
 }
